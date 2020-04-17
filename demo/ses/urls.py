f327c4143103208aa86c5ed2e16099e93245f50c
hjq <52118565@qq.com>
2020-04-17 21:42:48 +0800

    类视图和Minxin扩展类

--- demo/ses/urls.py
+++ demo/ses/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     re_path(r'setses/', views.setses),
     re_path(r'^getses/', views.getses),
+    # re_path(r'^rview/', views.Rview.as_view()),
+    # 类视图装饰器第二种方法
+    # re_path(r'^rview/', views.foo(views.Rview.as_view())),
+    re_path(r'^rview/', views.Rview.as_view()),
 ]
--- demo/ses/views.py
+++ demo/ses/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from django.utils.decorators import method_decorator
+from django.views import View
 
 
 def setses(request):
@@ -14,3 +16,50 @@ def getses(request):
     request.session.set_expiry(7)
     print(a)
     return HttpResponse("获取session")
+
+
+def foo1(fun):
+    def inner(request, *args, **kwargs):
+        print("我是装饰器1")
+        print(request.path)
+        return fun(request, *args, **kwargs)
+    return inner
+
+
+def foo2(fun):
+    def inner(request, *args, **kwargs):
+        print("我是装饰器2")
+        print(request.path)
+        return fun(request, *args, **kwargs)
+    return inner
+
+
+class FoMixin1(object):
+    @classmethod
+    def as_view(cls, *args, **kwargs):
+        view = super().as_view()
+        return foo1(view)
+
+
+class FoMixin2(object):
+    @classmethod
+    def as_view(cls, *args, **kwargs):
+        view = super().as_view()
+        return foo2(view)
+
+
+# 类视图装饰器第三种方法
+# @method_decorator(foo, name="dispatch")
+class Rview(FoMixin1, FoMixin2, View):
+
+    # 类视图装饰器第一种方法
+    # @foo
+    def get(self, request):
+        print("我是GET方法")
+        return HttpResponse("我只是GET方法")
+
+    def post(self, request):
+        print("我是POST方法")
+        return  HttpResponse("我是POST方法")
+
+
